42354cf7980f7bf027a2f23a50bcddcf3fa1b7ed
jaskarnmankoo <jaskarn.mankoo@mail.utoronto.ca>
2021-11-16 18:52:15 -0500

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  pathPrefix: '/guess-the-word',
+  plugins: [
+    'gatsby-plugin-csp',
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-preact',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-html-attributes',
+      options: {
+        lang: 'en'
+      }
+    }
+  ],
+  siteMetadata: {
+    title: 'Guess the Word',
+    description: 'Single player version of Guess the Word',
+    author: 'Jaskarn Mankoo'
+  }
+};
