1393c7c2d2f0f30a060c69b13f796c0fcd1314a8
jaskarnmankoo <jaskarn.mankoo@mail.utoronto.ca>
2021-12-18 17:46:43 -0500

    updated package.json and upgraded to tailwindv3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: { extend: { colors: { cream: '#f3f4f5', 'dark-mode': '#202124' } } }
 };
